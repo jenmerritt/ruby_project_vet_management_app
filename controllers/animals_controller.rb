d80a024a8b0a1482bf42c1abdcfeecd4b665b483
Jen Merritt <jenjomez@gmail.com>
2019-12-15 15:18:12 +0000

    setup animal view

--- controllers/animals_controller.rb
+++ controllers/animals_controller.rb
@@ -4,3 +4,8 @@ require_relative('../models/animal')
 require_relative('../models/owner')
 require_relative('../models/vet')
 also_reload('../models/*')
+
+get '/animals' do
+  @animals = Animal.all
+  erb(:"animals/index")
+end
