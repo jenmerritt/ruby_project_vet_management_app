b8f334e391393d6d4b797ffe54709db1af6c6d01
Jen Merritt <jenjomez@gmail.com>
2019-12-16 10:26:48 +0000

    vet edit completed

--- controllers/vets_controller.rb
+++ controllers/vets_controller.rb
@@ -8,7 +8,31 @@ get '/vets' do
   erb(:"vets/index")
 end
 
+get '/vets/new' do
+  erb (:"vets/new")
+end
+
+post '/vets/new' do
+  @vet = Vet.new(params)
+  @vet.save()
+  erb (:"vets/create")
+end
+
 get '/vets/:id' do
   @vet = Vet.find(params[:id])
   erb (:"vets/show")
+end
+
+# edit
+
+get '/vets/:id/edit' do
+  @vet = Vet.find(params[:id])
+  erb (:"vets/edit")
+end
+
+# update
+
+post '/vets/:id/edit' do
+  Vet.new(params).update()
+  erb (:"vets/create")
 end
