7b364efde0cb4e6ba37d4ff10a2a0e33eaa566ce
Jen Merritt <jenjomez@gmail.com>
2019-12-16 13:30:31 +0000

    appointments form edit.erb

--- controllers/appointments_controller.rb
+++ controllers/appointments_controller.rb
@@ -10,3 +10,37 @@ get '/appointments' do
   @appointments = Appointment.all
   erb(:"appointments/index")
 end
+
+get '/appointments/new' do
+  @appointments = Appointment.all
+  @vets = Vet.all
+  @animals = Animal.all
+  erb (:"appointments/new")
+end
+
+post '/appointments/new' do
+  @appointment = Appointment.new(params)
+  @appointment.save()
+  erb (:"appointments/create")
+end
+
+get '/appointments/:id' do
+  @appointment = Appointment.find(params[:id])
+  erb (:"appointments/show")
+end
+
+# edit
+
+get '/appointments/:id/edit' do
+  @appointment = Appointment.find(params[:id])
+  @animals = Animal.all
+  @vets = Vet.all
+  erb (:"appointments/edit")
+end
+
+# update
+
+post '/appointments/:id/edit' do
+  Appointment.new(params).update()
+  erb (:"appointments/create")
+end
